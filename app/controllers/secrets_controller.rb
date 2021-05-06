e26210e845ed7d1b6975f824ccf44418949d58d6
Jesper Sørensen <jeppester1@gmail.com>
2021-05-06 14:03:52 +0200

    Block various preview bots

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class SecretsController < ApplicationController
+  BLOCKED_BOTS = [
+    /facebookexternalhit/,
+    /Slackbot-LinkExpanding/,
+    /Twitterbot/,
+    /WhatsApp/,
+    /Discord/
+  ].freeze
+
   def new; end
 
-  def show; end
+  def show
+    should_block = BLOCKED_BOTS.any? do |bot|
+      bot =~ request.headers["HTTP_USER_AGENT"]
+    end
+
+    redirect_to(root_path) if should_block
+  end
 end
