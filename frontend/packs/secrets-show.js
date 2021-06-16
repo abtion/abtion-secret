28e9367af01965a024986ac93f8776473dfa50bc
Jesper Sørensen <jeppester1@gmail.com>
2021-06-16 22:31:02 +0200

    Implement and use react-rails-esque react component helper

--- frontend/packs/secrets-show.js
+++
@@ -1,7 +0,0 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-import React from "react"
-import ReactDOM from "react-dom"
-import SecretShow from "../pages/SecretShow"
-
-ReactDOM.render(React.createElement(SecretShow), document.querySelector("main"))
