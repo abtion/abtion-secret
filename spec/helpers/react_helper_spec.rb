87f31b22f43915919e33518f426bc5c99706656c
hcarreras <haricarreras@gmail.com>
2021-06-17 14:49:01 +0200

    Rubocop

--- spec/helpers/react_helper_spec.rb
+++ spec/helpers/react_helper_spec.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe ReactHelper do
   context "when passing React props" do
     it "renders the props in data " do
-      html = react_component('Foo', { bar: 'value' })
-      expected_props = %w(data-react-component="Foo" data-react-props="{&quot;bar&quot;:&quot;value&quot;}")
+      html = react_component("Foo", { bar: "value" })
+      expected_props = %w[data-react-component="Foo"
+                          data-react-props="{&quot;bar&quot;:&quot;value&quot;}"]
 
       expected_props.each do |segment|
         expect(html).to include(segment)
@@ -14,8 +17,9 @@ RSpec.describe ReactHelper do
 
   context "when passing caramelized props" do
     it "renders the props in data " do
-      html = helper.react_component('Foo', { foo_bar: 'value' })
-      expected_props = %w(data-react-component="Foo" data-react-props="{&quot;fooBar&quot;:&quot;value&quot;}")
+      html = helper.react_component("Foo", { foo_bar: "value" })
+      expected_props = %w[data-react-component="Foo"
+                          data-react-props="{&quot;fooBar&quot;:&quot;value&quot;}"]
 
       expected_props.each do |segment|
         expect(html).to include(segment)
