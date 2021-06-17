cb26523e151ee74532d71bd1ce1e58c9331cb945
hcarreras <haricarreras@gmail.com>
2021-06-17 16:40:13 +0200

    Add missing react helper specs
    Missing test coverage

--- spec/helpers/react_helper_spec.rb
+++ spec/helpers/react_helper_spec.rb
@@ -15,11 +15,47 @@ RSpec.describe ReactHelper do
     end
   end
 
-  context "when passing caramelized props" do
+  context "when passing camelized props" do
     it "renders the props in data " do
       html = helper.react_component("Foo", { foo_bar: "value" })
       expected_props = %w[data-react-component="Foo"
                           data-react-props="{&quot;fooBar&quot;:&quot;value&quot;}"]
+
+      expected_props.each do |segment|
+        expect(html).to include(segment)
+      end
+    end
+  end
+
+  context "when passing nested arrays" do
+    it "renders the props in data " do
+      html = helper.react_component("Foo",
+                                    { foo_bar: [{ user_name: "Jens" }, { user_name: "Jensen" }],
+                                      bar_foo: 1 })
+
+      expected_props = ['data-react-component="Foo"',
+                        'data-react-props="{&quot;fooBar&quot;:' \
+                          "[{&quot;userName&quot;:&quot;Jens&quot;}," \
+                          '{&quot;userName&quot;:&quot;Jensen&quot;}],&quot;barFoo&quot;:1}"']
+
+      expected_props.each do |segment|
+        expect(html).to include(segment)
+      end
+    end
+  end
+
+  context "when passing ActionController::Parameters" do
+    before do
+      ActionController::Parameters.permit_all_parameters = true
+    end
+
+    it "renders the props in data " do
+      params = ActionController::Parameters.new({ bar: "value" })
+
+      html = react_component("Foo", params)
+
+      expected_props = %w[data-react-component="Foo"
+                          data-react-props="{&quot;bar&quot;:&quot;value&quot;}"]
 
       expected_props.each do |segment|
         expect(html).to include(segment)
