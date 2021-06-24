fc16bc117f9aa87605bcaaa7051f269c3bfcbeac
Jesper Sørensen <jeppester1@gmail.com>
2021-06-24 22:49:13 +0200

    Don't include test files in require context

--- frontend/packs/react-components.js
+++ frontend/packs/react-components.js
@@ -1,5 +1,9 @@
 import mountComponents from "../utils/mountComponents"
 
-const pagesContext = require.context("../pages", true)
+const pagesContext = require.context(
+  "../pages",
+  true,
+  /(?<![/.]test(\.jsx?)?)$/
+)
 
 mountComponents(pagesContext)
