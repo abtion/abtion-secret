7c966daaca9ed0d5eae5cd439fa6e818658efa23
Jesper Sørensen <jeppester1@gmail.com>
2021-06-27 00:13:01 +0200

    eslint

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   roots: ["frontend"],
   testRegex: ".*test.jsx?$",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ['./frontend/jest.setup.js'],
+  setupFilesAfterEnv: ["./frontend/jest.setup.js"],
   moduleFileExtensions: ["js", "json", "jsx", "scss"],
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy",
