a4db02b1d83927fd2244f63ef166046225ea4155
Jesper Sørensen <jeppester1@gmail.com>
2025-06-12 06:51:52 +0000

    Upgrade to rails 8, based on commit from template

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "active_support/core_ext/integer/time"
-
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -10,22 +8,19 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = true
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = defined?(Spring)
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
-  }
+  # Configure public file server for tests with cache-control for performance.
+  config.public_file_server.headers = { "cache-control" => "public, max-age=3600" }
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # Show full error reports.
+  config.consider_all_requests_local = true
   config.cache_store = :memory_store
 
   # Raise exceptions instead of rendering exception templates.
@@ -36,12 +31,6 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
-
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,21 +1,8 @@
-const { generateWebpackConfig } = require("shakapacker")
+const { generateWebpackConfig, merge } = require("shakapacker")
 const webpackConfig = generateWebpackConfig()
 const webpack = require("webpack")
 require("./dotenv")
 
 webpackConfig.plugins.push(new webpack.EnvironmentPlugin(process.env))
 
-const nodeSassGlobImporter = require("node-sass-glob-importer")
-
-const sassRule = webpackConfig.module.rules.find((rule) =>
-  [].concat(rule.test).some((test) => test.test(".scss"))
-)
-const sassLoader = sassRule.use.find((use) => {
-  const loader = typeof use === "object" ? use.loader : use
-  return loader.includes("sass-loader")
-})
-
-sassLoader.options.sassOptions.importer = nodeSassGlobImporter()
-sassLoader.options.api = "legacy"
-
-module.exports = webpackConfig
+module.exports = merge({}, webpackConfig)
