836ece5f7d5b420796360a23151a0874baf7728c
Jesper Sørensen <jeppester1@gmail.com>
2025-11-12 09:49:10 +0000

    Fix jest

--- config/swc.config.js
+++ config/swc.config.js
@@ -3,6 +3,9 @@ const { env } = require("shakapacker")
 const customConfig = {
   options: {
     jsc: {
+      parser: {
+        jsx: true, // Needed for jest
+      },
       transform: {
         react: {
           development: env.isDevelopment,
